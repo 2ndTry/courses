271aa79a2663aea95bd2de8b12e3388efdcb1254
Alexey Mirniy <mirnijalex@gmail.com>
2024-03-07 15:24:49 +0300

    Add Swagger server

--- src/main/java/com/mirniy/courses/CoursesApplication.java
+++ src/main/java/com/mirniy/courses/CoursesApplication.java
@@ -1,8 +1,15 @@
 package com.mirniy.courses;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://alexeymirniy.ru/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 public class CoursesApplication {
     public static void main(String[] args) {
